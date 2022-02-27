3f5802945d4f4d64f0d2e1770984b64facf560cb
vitorcosta039 <vitorcosta588@gmail.com>
2022-02-26 23:29:14 -0300

    refactor: Componete links menu

---
+++ components/Header/HeaderLink.vue
@@ -0,0 +1,57 @@
+<template>
+  <a
+    :href="link"
+    class="
+      header-link
+      duration-200
+      ease-in
+      flex
+      lg:mx-4
+      py-3
+      lg:py-1.5
+      relative
+      text-white
+      lg:text-gray-500
+      lg:hover:text-gray-600
+      lg:dark:text-gray-400
+      lg:dark:hover:text-white
+    "
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'HeaderLink',
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 1024px) {
+  .header-link {
+    &::after {
+      background: $gray-400;
+      border-radius: 2px;
+      bottom: 2px;
+      content: '';
+      height: 2px;
+      position: absolute;
+      transition: all 300ms;
+      width: 0;
+    }
+
+    &:hover {
+      &::after {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
---
+++ components/Header/HeaderSocialLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <a
+    class="
+      duration-200
+      ease-in
+      flex
+      lg:hover:bg-primary
+      p-3
+      lg:p-2
+      rounded-full
+    "
+    :href="link"
+    target="_black"
+    :title="title"
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSocialLink',
+  props: {
+    link: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
