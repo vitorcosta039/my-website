5d9e7f97f45f36b387762ddd558703a7d8241f28
vitorcosta039 <vitorcosta588@gmail.com>
2022-03-01 20:48:00 -0300

    chore: Configs general project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
-import { resolve } from 'path'
+import { resolve } from 'path';
 import i18n from './config/i18n';
 
 export default {
   target: 'static',
 
   alias: {
-    'images': resolve(__dirname, './assets/images')
+    images: resolve(__dirname, './assets/images')
   },
 
   head: {
@@ -13,10 +13,33 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'author', name: 'author', content: 'Vitor Costa' },
+      { hid: 'description', name: 'description', content: 'Olá, meu nome é Vitor Costa, sou Dev Front-end.' },
+      { name: 'keys', content: 'front-end, developer, frontend developer, desenvolvedor frontend, vuejs, vue, js, nuxt, nuxtjs, web, site, website' },
+
+      // OpenGraph
+      { name: 'og:locale', content: 'pt_BR' },
+      { name: 'og:url', content: 'https://vitorcosta.dev' },
+      { name: 'og:title', content: 'Vitor Costa - Dev Front-end' },
+      { name: 'og:site_name', content: 'Vitor Costa' },
+      { name: 'og:description', content: 'Olá, meu nome é Vitor Costa, sou Dev Front-end.' },
+      { name: 'og:image', content: 'https://vitorcosta.dev/logo-lg.png' },
+      { name: 'og:image:type', content: 'image/png' },
+      { name: 'og:image:width', content: '1000' },
+      { name: 'og:image:height', content: '1000' },
+      { name: 'og:type', content: 'website' },
+
+      // Twitter
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:url', content: 'https://vitorcosta.dev' },
+      { name: 'twitter:title', content: 'Vitor Costa - Dev Front-end' },
+      { name: 'twitter:description', content: 'Olá, meu nome é Vitor Costa, sou Dev Front-end.' },
+      { name: 'twitter:image', content: 'https://vitorcosta.dev/logo-lg.png' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
 
   css: ['./assets/style/main.scss'],
@@ -39,7 +62,6 @@ export default {
         }
       }
     ],
-    '@nuxt-hero-icons/outline/nuxt',
     [
       '@nuxtjs/style-resources',
       {
@@ -64,19 +86,28 @@ export default {
     [
       '@nuxtjs/i18n',
       {
-        vueI18nLoader: true,
+        baseUrl: 'https://vitorcosta.dev',
+        detectBrowserLanguage: {
+          useCookie: true,
+          cookieKey: 'i18n_redirected',
+          redirectOn: 'root',
+        },
         defaultLocale: 'en',
         locales: [
           {
             code: 'en',
+            iso: 'en-US',
             name: 'English'
           },
           {
             code: 'br',
+            iso: 'pt-BR',
             name: 'Português'
           }
         ],
-        vueI18n: i18n
+        seo: true,
+        vueI18n: i18n,
+        vueI18nLoader: true
       }
     ],
     'v-wave/nuxt'
@@ -87,6 +118,8 @@ export default {
       lang: 'en'
     }
   },
+
+  plugins: [{ src: '~/plugins/scrollreveal', ssr: false }],
 
   server: {
     port: 8000,
