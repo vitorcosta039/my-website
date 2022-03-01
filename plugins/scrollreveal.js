8775c86029b9b3a9e7be7fb9782190af8a156f88
vitorcosta039 <vitorcosta588@gmail.com>
2022-03-01 20:47:35 -0300

    chore: Add scrolreveal

---
+++ plugins/scrollreveal.js
@@ -0,0 +1,20 @@
+import ScrollReveal from 'scrollreveal';
+
+const fadeInTop20 = {
+  delay: 400,
+  distance: '-20px',
+  duration: 600,
+  reset: true
+};
+
+const elsfadeInTop20 = document.querySelectorAll('[data-scroll-anim="fadeInTop20"]');
+ScrollReveal().reveal(elsfadeInTop20, fadeInTop20);
+
+
+const elsfadeInTop20Delay = document.querySelectorAll('[data-scroll-anim-delay="fadeInTop20"]');
+elsfadeInTop20Delay.forEach((el, index) => {
+  ScrollReveal().reveal(el, {
+    ...fadeInTop20,
+    delay: index * 200
+  });
+});
